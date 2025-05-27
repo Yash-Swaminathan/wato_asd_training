bfef4530ef7e74853e805620673449b822e33f9a
Yash Swaminathan <yswaminathan@theturingcompany.com>
2025-05-27 18:47:39 -0400

    fixed planner .hpp files

--- src/robot/planner/include/planner_node.hpp
+++ src/robot/planner/include/planner_node.hpp
@@ -1,43 +1,70 @@
 #ifndef PLANNER_NODE_HPP_
 #define PLANNER_NODE_HPP_
 
-#include "planner_core.hpp"
-#include "rclcpp/rclcpp.hpp"
-#include "nav_msgs/msg/path.hpp"
-#include "nav_msgs/msg/occupancy_grid.hpp"
-#include "geometry_msgs/msg/pose_stamped.hpp"
-#include "nav_msgs/msg/odometry.hpp"
 #include <mutex>
 
+#include "rclcpp/rclcpp.hpp"
+#include "nav_msgs/msg/occupancy_grid.hpp"
+#include "nav_msgs/msg/path.hpp"
+#include "nav_msgs/msg/odometry.hpp"
+#include "geometry_msgs/msg/point_stamped.hpp"
+
+#include "planner_core.hpp"
+
 class PlannerNode : public rclcpp::Node {
-public:
-  PlannerNode();
+  public:
+    PlannerNode();
 
-private:
-  void getParameters();
-  void mapCallback(const nav_msgs::msg::OccupancyGrid::SharedPtr map_msg);
-  void goalCallback(const geometry_msgs::msg::PoseStamped::SharedPtr goal_msg);
-  void odomCallback(const nav_msgs::msg::Odometry::SharedPtr odom_msg);
-  void timerCallback();
-  void publishPath();
-  void resetGoal();
+    void processParameters();
 
-  robot::PlannerCore planner_;
-  rclcpp::Subscription<nav_msgs::msg::OccupancyGrid>::SharedPtr map_sub_;
-  rclcpp::Subscription<geometry_msgs::msg::PoseStamped>::SharedPtr goal_sub_;
-  rclcpp::Subscription<nav_msgs::msg::Odometry>::SharedPtr odom_sub_;
-  rclcpp::Publisher<nav_msgs::msg::Path>::SharedPtr path_pub_;
-  rclcpp::TimerBase::SharedPtr timer_;
-  nav_msgs::msg::OccupancyGrid::SharedPtr map_;
-  std::mutex map_mutex_;
-  geometry_msgs::msg::PoseStamped current_goal_;
-  bool active_goal_{false};
-  rclcpp::Time plan_start_time_;
-  bool have_odom_{false};
-  double odom_x_{0.0}, odom_y_{0.0};
-  double smoothing_factor_{0.0};
-  int iterations_{0};
-  double goal_tolerance_{0.0}, plan_timeout_{0.0};
+    void mapCallback(const nav_msgs::msg::OccupancyGrid::SharedPtr map_msg);
+    void goalCallback(const geometry_msgs::msg::PointStamped::SharedPtr  goal_msg);
+    void odomCallback(const nav_msgs::msg::Odometry::SharedPtr odom_msg);
+    void timerCallback();
+
+    void publishPath();
+    void resetGoal();
+
+  private:
+    robot::PlannerCore planner_;
+
+    // Subscriptions
+    rclcpp::Subscription<nav_msgs::msg::OccupancyGrid>::SharedPtr map_sub_;
+    rclcpp::Subscription<geometry_msgs::msg::PointStamped>::SharedPtr goal_sub_;
+    rclcpp::Subscription<nav_msgs::msg::Odometry>::SharedPtr odom_sub_;
+
+    // Publisher
+    rclcpp::Publisher<nav_msgs::msg::Path>::SharedPtr path_pub_;
+
+    // Timer
+    rclcpp::TimerBase::SharedPtr timer_;
+
+    // Costmap & Mutex
+    nav_msgs::msg::OccupancyGrid::SharedPtr map_;
+    std::mutex map_mutex_;
+
+    // Current goal
+    geometry_msgs::msg::PointStamped current_goal_;
+    bool active_goal_;
+    rclcpp::Time plan_start_time_;
+
+    // Robot odometry (x,y). For simplicity, ignoring orientation usage here.
+    bool have_odom_;
+    double odom_x_;
+    double odom_y_;
+
+    // Parameters
+    std::string map_topic_;
+    std::string goal_topic_;
+    std::string odom_topic_;
+    std::string path_topic_;
+
+    double smoothing_factor_;
+    int iterations_;
+
+    double goal_tolerance_;
+    double plan_timeout_;
+
 };
 
-#endif  
+#endif 
